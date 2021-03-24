6ba285d438db41b0f7019e9add2ba615cd84ada2
nichoth <nichoth@gmail.com>
2021-03-24 16:05:13 -0700

    trying things

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-
 var gun = Gun([
     'http://localhost:8765/gun',
     'https://mvp-gun.herokuapp.com/gun',
     'https://e2eec.herokuapp.com/gun'
 ])
+
+var user = gun.user()
+
+$('#up').on('click', function(e){
+    user.create($('#alias').val(), $('#pass').val())
+})
+
+$('#sign').on('submit', function(e){
+    e.preventDefault()
+    user.auth($('#alias').val(), $('#pass').val())
+})
 
